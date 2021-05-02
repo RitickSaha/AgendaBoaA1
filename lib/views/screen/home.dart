409fc8e430bc6c9949e82ff56215ff5f664fbca2
Ritick Saha <RitickSaha@users.noreply.github.com>
2021-05-03 00:58:10 +0530

    ll

--- lib/views/screen/home.dart
+++ lib/views/screen/home.dart
@@ -1,11 +1,11 @@
 import 'package:agenda_boa_assignemnt/api/gitSearch_api.dart';
 import 'package:agenda_boa_assignemnt/bloc/GitRepo_bloc.dart';
 import 'package:agenda_boa_assignemnt/model/gitRepoModel.dart';
+import 'package:agenda_boa_assignemnt/views/widgets/LoadingWidget.dart';
 import 'package:agenda_boa_assignemnt/views/widgets/RepoWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:glassmorphism/glassmorphism.dart';
-import 'package:shimmer/shimmer.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -26,7 +26,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-
     newMethod();
     gitRepoBloc = GitRepoBloc(
       sort: selectedSort,
@@ -152,98 +151,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               return Center(child: CircularProgressIndicator());
                             },
                           ),
-                          if (isLoading)
-                            Shimmer.fromColors(
-                              baseColor: Colors.white.withOpacity(0.001),
-                              highlightColor: Colors.white.withOpacity(0.2),
-                              child: Column(
-                                  children: List.generate(
-                                3,
-                                (index) => Container(
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      ClipOval(
-                                        child: Container(
-                                          width: 68.0,
-                                          height: 68.0,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        children: [
-                                          Container(
-                                            width: MediaQuery.of(context)
-                                                    .size
-                                                    .width *
-                                                0.7,
-                                            height: 16,
-                                            decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(4)),
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            height: 4,
-                                          ),
-                                          Container(
-                                            width: MediaQuery.of(context)
-                                                    .size
-                                                    .width *
-                                                0.6,
-                                            height: 16,
-                                            decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(4)),
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            height: 4,
-                                          ),
-                                          Container(
-                                            width: MediaQuery.of(context)
-                                                    .size
-                                                    .width *
-                                                0.5,
-                                            height: 16,
-                                            decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(4)),
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            height: 4,
-                                          ),
-                                          Container(
-                                            width: MediaQuery.of(context)
-                                                    .size
-                                                    .width *
-                                                0.7,
-                                            height: 15,
-                                            decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(4)),
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            height: 4,
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              )),
-                            ),
+                          if (isLoading) LoadingWidget(),
                         ],
                       ),
                     ),
